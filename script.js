70aa237888288464620bee913d0c7aa88c7ceaa5
Mayank Namdev <117892477+mayanknagithub@users.noreply.github.com>
2023-03-26 21:49:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+	let str1 = '';
+	for (let index = str.length-1; index >= 0; index--) {
+		str1.concate(charAt(index));
+	}
+	if(str==str1){
+		console.log(str,'is a palindrome');
+	}
+	else{
+		console.log(str,'is not a palindrome');
+	}
 }
 module.exports = palindrome
