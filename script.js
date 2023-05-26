3c124062447d540f6d9282d166fffd77e90c629b
Mayank Namdev <117892477+mayanknagithub@users.noreply.github.com>
2023-05-26 18:50:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-	let str1 = '';
-	for (let index = str.length-1; index >= 0; index--) {
-		str1.concate(charAt(index));
+	let isPalindrome = true;
+	let i = 0;
+	let j = str.length-1;
+
+	while(i<=j){
+		if(str[i]!=str[j]) {
+			isPalindrome = false;
+		}
+		i++;
+		j--;
 	}
-	if(str==str1){
-		return true;
-	}
-	else{
-		return false;
-	}
+	return isPalindrome;
 }
 
 module.exports = palindrome
